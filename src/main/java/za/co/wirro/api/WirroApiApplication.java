dfcb3591099844e21d0c9b6582ea262d6807fb4f
Kgopolomo <Kgopolomo@gmail.com>
2023-07-25 14:02:46 +0200

    initial commit

---
+++ src/main/java/za/co/wirro/api/WirroApiApplication.java
@@ -0,0 +1,13 @@
+package za.co.wirro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirroApiApplication.class, args);
+	}
+
+}
